1f3ae3b6f2d723c8515bf42aabeb7a832c549503
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-23 10:41:06 +0100

    INIT

---
+++ src/components/SideNav/SideNav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SideNav.scss";
+
+const SideNav = () => {
+  return (
+    <div className='sidenav'>
+      <p>ALL BOARDS (2)</p>
+    </div>
+  );
+};
+
+export default SideNav;
